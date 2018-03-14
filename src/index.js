602288c1b08941161583798625aa958f8a190036
jicjjang <jicjjang12@gmail.com>
2018-03-15 02:23:10 +0900

    modified upload file to queue format

--- src/index.js
+++ src/index.js
@@ -3,34 +3,6 @@ const app = express()
 let port = 3000
 
 /**
- * @DB
- * @Description 거래내역 저장으로 확장 가능
- */
-// import mongoose from 'mongoose'
-// import DB_URL from './config'
-// mongoose.connect(DB_URL)
-// mongoose.Promise = global.Promise
-// mongoose.set('debug', true)
-//
-// const db = mongoose.connection
-// db.on('error', console.error.bind(console, 'connection error:'))
-// db.once('open', console.log.bind(console, "Connected to mongod server"))
-
-/**
- * @Route
- * @Description static 파일 또는 일반 경로, 에러처리 (소켓만 사용하면 거의 사용 용도가 없음.)
- */
-// import routes from './routes'
-// app.use('/static', express.static(__dirname + '/../public'))
-// app.use('/', routes)
-// app.use((err, req, res, next) => {
-//   res.status(err.status || 500).send({
-//     code: err.status || 500,
-//     message: err.message || 'Server error'
-//   })
-// })
-
-/**
  * @Server
  * @Description 서버
  */
@@ -46,5 +18,4 @@ const server = app.listen(port, () => {
  * @params initialSellList : 임의의 초기 데이터
  */
 
-import { initialBuyList, initialSellList } from './initTradeList'
-require('./io')(server, initialBuyList, initialSellList)
+require('./io')(server)
--- src/io.js
+++ src/io.js
@@ -1,4 +1,13 @@
 import { tradeResultEmitter } from './modules/trader'
+import { initialBuyList as buyList, initialSellList as sellList } from './initTradeList'
+
+/**
+ * @tempFileSaveArea
+ * @Description DB 대신 입력된 파일들을 순차적으로 저장해놓을 수 있는 임시 변수. 저장할 곳이 없으면
+ *              입력되는 값들이 소켓마다 async하게 값들을 계속 보내서 1초마다 실행이 되지 않음.
+ */
+let tempFileSaveArea = []
+
 /**
  * @io.js
  * @param server
@@ -13,25 +22,47 @@ import { tradeResultEmitter } from './modules/trader'
  *    count: Int
  *  }]
  */
-module.exports = (server, buyList, sellList) => {
+module.exports = (server) => {
   const io = require('socket.io')(server)
   io.on('connection', socket => {
+    console.log(`${socket.id} connected`)
+
     socket.emit('init data parse', { buyList, sellList })
 
     socket.on('file parser', files => {
-      let index = 0
-
-      tradeResultEmitter(io, files[index++], buyList, sellList)
-      let fileDataSender = setInterval(() => {
-        if (!files[index] || files[index] == undefined) {
-          clearInterval(fileDataSender)
-        }
-        tradeResultEmitter(io, files[index++], buyList, sellList)
-      }, 1000)
+      // 파일을 추가 전, 기존에 이미 tempFileWatcher가 실행 중이니 리스트만 추가
+      // 리스트가 없으면 실행 중이 아니니 tempFileWatcher를 실행
+      if (tempFileSaveArea && tempFileSaveArea.length > 0) {
+        tempFileSaveArea = tempFileSaveArea.concat(files.split('\n'))
+      } else {
+        tempFileSaveArea = tempFileSaveArea.concat(files.split('\n'))
+        tempFileWatcher(io)
+      }
     })
 
     socket.on('disconnect', () => {
       console.log(`${socket.id} disconnected`)
     })
   })
+}
+
+function tempFileWatcher (io) {
+  if (!tempFileSaveArea || tempFileSaveArea.length <= 0) {
+    return
+  }
+
+  const data = tempFileSaveArea.shift()
+  if (!data) {
+    return
+  }
+  tradeResultEmitter(io, data, buyList, sellList)
+
+  const fileDataSender = setInterval(() => {
+    const data = tempFileSaveArea.shift()
+    if (!data) {
+      clearInterval(fileDataSender)
+      return
+    }
+    tradeResultEmitter(io, data, buyList, sellList)
+  }, 1000)
 }
